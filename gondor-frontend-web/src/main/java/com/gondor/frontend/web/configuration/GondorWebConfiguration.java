84255156c8ff1e4b6534caedb806630faa4f56e8
犹似缘 <mijinchenyuan@163.com>
2020-07-03 18:01:39 +0800

    springboot集成Apollo配置

--- gondor-frontend-web/src/main/java/com/gondor/frontend/web/configuration/GondorWebConfiguration.java
+++ gondor-frontend-web/src/main/java/com/gondor/frontend/web/configuration/GondorWebConfiguration.java
@@ -1,9 +1,23 @@
 package com.gondor.frontend.web.configuration;
 
+import com.ctrip.framework.apollo.spring.annotation.EnableApolloConfig;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 
+/**
+ *TODO：配置Apollo
+ * 设置VM options参数，示例如下：
+ * (1)设置读取dev环境的Apollo配置，无默认值
+ *    -Denv=dev
+ * (2)设置读取哪个集群的配置，默认default
+ *    -Dapollo.cluster=default
+ */
+@EnableApolloConfig
+@EnableEurekaClient
+@EnableFeignClients(basePackages = {"com.gondor.frontend.client"})
 @Configuration
 @Import(value = {BalanceConfig.class, JacksonConfig.class})
 @ComponentScan(basePackages = {"com.gondor.frontend.fallbackfactory"})
--- gondor-frontend-web/src/main/java/com/gondor/frontend/web/controller/GondorWebController.java
+++ gondor-frontend-web/src/main/java/com/gondor/frontend/web/controller/GondorWebController.java
@@ -4,19 +4,28 @@ import com.gondor.frontend.dto.Customer;
 import com.gondor.frontend.dto.Product;
 import com.gondor.frontend.web.controller.base.SuperController;
 import com.gondor.master.utils.JsonUtils;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Controller
 @RequestMapping("/web/test")
 public class GondorWebController extends SuperController {
 
     private static final String KEY = "3BFBZ-ZKD3X-LW54A-ZT76D-E7AHO-4RBD5";
+
+    //TODO：
+    // 使用@Value注解读取Apollo中的配置test.apollo.info
+    // 这里将@Value读取不到配置时的默认值设置为NULL
+    @Value("${test.apollo.info:NULL}")
+    private String valueInApollo;
 
     @GetMapping("/p/list")
     @ResponseBody
@@ -38,16 +47,24 @@ public class GondorWebController extends SuperController {
         return str;
     }
 
-    @GetMapping("ex/{id}")
+    @GetMapping("/ex/{id}")
     @ResponseBody
     public String exceptionTest1(@PathVariable Integer id) {
         return productApiClient.exceptionTest(id);
     }
 
-    @GetMapping("err/{id}")
+    @GetMapping("/err/{id}")
     @ResponseBody
     public String exceptionTest2(@PathVariable Integer id) {
         return customerApiClient.exceptionTest(id);
+    }
+
+    @GetMapping("/apollo")
+    @ResponseBody
+    public Map<String, Object> apolloTest() {
+        Map<String, Object> apolloConfig = new HashMap<>();
+        apolloConfig.put("test.apollo.info", valueInApollo);
+        return apolloConfig;
     }
 
 }
